66ad3d3f20eccd06846a208c17008a54cc0e7ace
ekater1na <ekaterina_l@tut.by>
2021-02-27 20:45:23 +0300

    build: add components, interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponentComponent } from './book-component/book-component.component';
+import { CartComponentComponent } from './cart-component/cart-component.component';
+import { CartItemComponentComponent } from './cart-item-component/cart-item-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponentComponent,
+    CartComponentComponent,
+    CartItemComponentComponent
   ],
   imports: [
     BrowserModule,
