291945484c977b692322693511a81e37c5b706db
ekater1na <ekaterina_l@tut.by>
2021-02-27 21:05:10 +0300

    refactor: refactor project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookComponentComponent } from './book-component/book-component.component';
-import { CartComponentComponent } from './cart-component/cart-component.component';
-import { CartItemComponentComponent } from './cart-item-component/cart-item-component.component';
+import { BookComponentComponent } from './components/book-component/book-component.component';
+import { CartComponentComponent } from './components/cart-component/cart-component.component';
+import { CartItemComponentComponent } from './components/cart-item-component/cart-item-component.component';
 
 @NgModule({
   declarations: [
--- src/app/book-model.ts
+++ src/app/book-model.ts
@@ -1,2 +1,15 @@
+
+export enum EnumCategory {
+  Newspaper,
+  Newsletter,
+  Magazine,
+  Book
+};
 export interface BookModel {
-}
+  name: string;
+  description: string;
+  price: number;
+  category: EnumCategory;
+  createDate: number;
+  isAvailable: boolean;
+};
